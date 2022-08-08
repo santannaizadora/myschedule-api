4ea5371010c3bc0b31371b36e127dcefd4bfcf42
santannaizadora <santannaizadora@gmail.com>
2022-08-08 15:51:19 -0300

    fix: allow empty observation

--- src/schemas/appointment.schema.ts
+++ src/schemas/appointment.schema.ts
@@ -10,7 +10,7 @@ export const appointmentSchema = Joi.object<CreateAppointmentData>({
 	date: Joi.date().format("DD-MM-YYYY").required().messages({
 		"date.format": "Data deve ser no formato DD-MM-YYYY",
 	}),
-	observation: Joi.string(),
+	observation: Joi.string().allow(null, ''),
 	place: Joi.string().required(),
 	initial_time: Joi.string()
 		.pattern(/^([01]?[0-9]|2[0-3]):[0-5][0-9]$/)
