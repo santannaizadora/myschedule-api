4fdafd7cc9b4eacb983c0cf80f226573835d1489
santannaizadora <santannaizadora@gmail.com>
2022-08-09 12:47:08 -0300

    feat: user options

--- src/routers/user.router.ts
+++ src/routers/user.router.ts
@@ -1,5 +1,11 @@
 import { Router } from "express";
+import { validateToken } from "../middlewares/validateToken.js";
+import { update, deleteUser } from "../controllers/user.controller.js";
 
 const router = Router();
+
+router.use(validateToken);
+router.put("/:id", update);
+router.delete("/:id", deleteUser);
 
 export default router;
